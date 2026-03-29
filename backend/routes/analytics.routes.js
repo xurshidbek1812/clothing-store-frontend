a1472cae6e19f95c8dcda86b6946b7027a67c6ef
Bekchonov Xurshidbek <bekchonovxurshidbek3@gmail.com>
2026-03-29 13:54:53 +0500

    Delete backend directory

--- backend/routes/analytics.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getDashboardStats } from '../controllers/analytics.controller.js';
-import { verifyToken } from '../middleware/auth.middleware.js';
-
-const router = express.Router();
-router.get('/stats', verifyToken, getDashboardStats);
-
-export default router;
